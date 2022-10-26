4dcbfaaf0b3d1267a74f135f685819fcd41eb6c9
Hyunjong Kim <hjkim0811@snu.ac.kr>
2022-10-26 15:50:36 +0900

    change argument name

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Argparse Tutorial')
 
-    parser.add_argument('--table', '-t', action='store_true', default=False)
-    parser.add_argument('--image', '-i', action='store_true', default=False)
-    parser.add_argument('--caption', '-c', action='store_true', default=False)
+    parser.add_argument('--text', '-text', action='store_true', default=False)
+    parser.add_argument('--table', '-table', action='store_true', default=False)
+    parser.add_argument('--image', '-image', action='store_true', default=False)
+    parser.add_argument('--caption', '-caption', action='store_true', default=False)
 
     parser.add_argument('--pdf_dir', '-dir', type=str, default='input')
     parser.add_argument('--save_dir', '-save', type=str, default='output')
